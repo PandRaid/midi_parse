0edd4b11540f1d735885d5d0d8d9789300db1673
David Dunning <ilovebouldering@gmail.com>
2018-08-31 21:34:55 -0700

    Added initial defines and file open

--- parse_midi.c
+++ parse_midi.c
@@ -1,7 +1,7 @@
 /*
    Functions responsible for taking a MIDI file as an input and producing the 
    corresponding PIANOREADER specific format that encodes lights with timing
-   */
+*/
 
 #include <stdio.h>
 #include <file.h>
@@ -19,10 +19,19 @@
 #define MAX_MIDI_SIZE           322146
 #define HEADER_ASCII            0x4D546864
 
+////////////////////////////////////////////////////////////////////////////
+///////////////            Misc. Defines           /////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+
 
 ////////////////////////////////////////////////////////////////////////////
 
 int main(){
+  FILE* midi_handle;
 
+  midi_handle=fopen(FILE_NAME);
+  if(midi_handle){
 
+  }
 }
