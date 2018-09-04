57157f987806ac0a21735653af4468ffba62e73e
David Dunning <ilovebouldering@gmail.com>
2018-09-03 22:32:28 -0700

    Added file opening, and reading to a buffer

--- parse_midi.c
+++ parse_midi.c
@@ -4,13 +4,14 @@
 */
 
 #include <stdio.h>
-#include <file.h>
-#include <stlib.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "midi.h"
 
 ////////////////////////////////////////////////////////////////////////////
 ///////////////            Error Defines          //////////////////////////
 ////////////////////////////////////////////////////////////////////////////
-#define THROW_FILE_NOT_FOUND    -1
+#define FILE_NOT_FOUND          -1
 #define BAD_HEADER              -2
 
 ////////////////////////////////////////////////////////////////////////////
@@ -22,7 +23,7 @@
 ////////////////////////////////////////////////////////////////////////////
 ///////////////            Misc. Defines           /////////////////////////
 ////////////////////////////////////////////////////////////////////////////
-
+#define FILE_NAME               "/home/pandaman/Downloads/for_elise_by_beethoven.mid"
 
 
 ////////////////////////////////////////////////////////////////////////////
@@ -30,8 +31,30 @@
 int main(){
   FILE* midi_handle;
 
-  midi_handle=fopen(FILE_NAME);
+  midi_handle=fopen(FILE_NAME, "rb");
   if(midi_handle){
+    char midi_buffered[MAX_MIDI_SIZE];
+    uint32_t header_word, len;
+    uint32_t time_scale;
 
+    size_t file_len = fread(midi_buffered, sizeof(char), MAX_MIDI_SIZE, midi_handle);
+    if (ferror(midi_handle) != 0){
+      fprintf(stderr, "Error reading %s", FILE_NAME);
+    }
+    else{
+      midi_buffered[file_len++] = '\0';
+    }
+    fclose(midi_handle);
+    print_buffer_string(midi_buffered, MAX_MIDI_SIZE);
+  }
+  else{
+    fprintf(stderr, "Error opening midi file!\n");
+    return FILE_NOT_FOUND;
+  }
+}
+
+void print_buffer_string(char* str, uint32_t len){
+  for(int i=0; i<len; i++){
+    fprintf(stderr, "%c", str[i]);
   }
 }
